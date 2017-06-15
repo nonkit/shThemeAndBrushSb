d05c66ef89fa8df88dbceb805667e3de83c09a4c
Nonki Takahashi <nobukit@jcom.home.ne.jp>
2017-06-15 22:16:49 +0900

    the first version

---
+++ README.txt
@@ -0,0 +1,18 @@
+# shThemeSb.css and shBrushSb.js
+Small Basic brush module and theme for SyntaxHighlighter 3.0.83.
+
+## Usage
+Describe following brushes and themes in your HTML file.
+```
+  <script type="text/javascript" src="scripts/shCore.js"></script>
+  <script type="text/javascript" src="scripts/shBrushSb.js"></script>
+  <link type="text/css" rel="stylesheet" href="styles/shCore.css"/>
+  <link type="text/css" rel="stylesheet" href="styles/shThemeSb.css"/>
+```
+Then it can be used with one ofe these aliases:
+```
+<pre class="brush: sb;">
+<pre class="brush: smallbasic;">
+```
+
+See index.html as a sample.
---
+++ scripts/shBrushSb.js
@@ -0,0 +1,53 @@
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/SyntaxHighlighter
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * http://alexgorbatchev.com/SyntaxHighlighter/donate.html
+ *
+ * @version
+ * 3.0.83 (July 02 2010)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2010 Alex Gorbatchev.
+ * Copyright (C) 2017 Nonki Takahashi.
+ *
+ * @license
+ * Dual licensed under the MIT and GPL licenses.
+ */
+;(function()
+{
+	// CommonJS
+	typeof(require) != 'undefined' ? SyntaxHighlighter = require('shCore').SyntaxHighlighter : null;
+
+	function Brush()
+	{
+		var keywords =	'Else ElseIf EndFor EndIf EndSub EndWhile ' +
+						'For Goto If Step Sub Then To While';
+
+		var objects =	'Array Clock Control Desktop Dictionary ' +
+						'File Flickr GraphicsWindow ImageList Math Mouse ' +
+						'Network Program Shapes Sound Stack ' +
+						'Text TextWindow Timer Turtle';
+
+		this.regexList = [
+			{ regex: /'.*$/gm,										css: 'comments' },		// one line comments
+			{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,	css: 'string' },		// strings
+			{ regex: new RegExp(this.getKeywords(keywords), 'g'),	css: 'keyword' },		// sb keyword
+			{ regex: /(\b\d+[\.]?\d*\b|\.\d+\b)/g,					css: 'value' },   		// sb number
+			{ regex: /And|Or|\+|\-|\*|\/|&lt;|=|>|\(|\)|\.|\,/g,	css: 'color1' },		// sb operator
+			{ regex: new RegExp(this.getKeywords(objects), 'g'),	css: 'constants' },		// sb object
+			{ regex: /\.\b\w+\b/g,									css: 'preprocessor' }	// sb property,operation,event
+			];
+
+		this.forHtmlScript(SyntaxHighlighter.regexLib.aspScriptTags);
+	};
+
+	Brush.prototype	= new SyntaxHighlighter.Highlighter();
+	Brush.aliases	= ['sb', 'smallbasic'];
+
+	SyntaxHighlighter.brushes.Sb = Brush;
+
+	// CommonJS
+	typeof(exports) != 'undefined' ? exports.Brush = Brush : null;
+})();
